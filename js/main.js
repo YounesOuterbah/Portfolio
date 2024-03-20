03a27ba380c7d816aaa0b5e5dd9be25483207563
Younes Outerbah <younesouterbah1@gmail.com>
2024-03-20 17:57:49 +0100

    fix randomize text function

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let interval = null;
-
-document.querySelector("span").onmouseover = (event) => {
+const target = document.querySelector("span");
+function randomizeText() {
+  let interval = null;
   let iteration = 0;
 
   clearInterval(interval);
 
   interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+    target.innerText = target.innerText
       .split("")
       .map((letter, index) => {
         if (index < iteration) {
-          return event.target.dataset.value[index];
+          return target.dataset.value[index];
         }
 
         return letters[Math.floor(Math.random() * 26)];
       })
       .join("");
 
-    if (iteration >= event.target.dataset.value.length) {
+    if (iteration >= target.dataset.value.length) {
       clearInterval(interval);
     }
 
     iteration += 1 / 3;
   }, 60);
-};
+}
+target.addEventListener("mouseover", () => {
+  randomizeText();
+});
+window.onload = randomizeText;
